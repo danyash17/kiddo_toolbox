d81a7acb7b3e96e7bc48380b8d2aa367ce2dc051
daniil <danikshkabara@tut.by>
2024-01-17 10:58:08 +0300

    Banner

--- project/src/toolbox/backdoor_client.py
+++ project/src/toolbox/backdoor_client.py
@@ -1,3 +1,9 @@
 import socket
 
+from toolbox.print.banner import *
+from toolbox.print.clprinter import *
+
+print_horizontal_delimiter()
+print_backdoor_banner()
+print_horizontal_delimiter()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- project/src/toolbox/print/banner.py
+++ project/src/toolbox/print/banner.py
@@ -3,8 +3,14 @@ import termcolor
 import os
 
 
-def print_portscanner_banner(color="green"):
-    with open(str(f"{os.path.dirname(os.path.abspath(__file__))}/banner.txt"), mode="r") as banner:
+def print_portscanner_banner():
+    do_print("/banner.txt")
+
+def print_backdoor_banner():
+    do_print("/backdoor_banner.txt")
+
+def do_print(banner_path, trollface_path = "/trollface.txt"):
+    with open(str(f"{os.path.dirname(os.path.abspath(__file__))}{banner_path}"), mode="r") as banner:
         print(banner.read())
-    with open(str(f"{os.path.dirname(os.path.abspath(__file__))}/trollface.txt"), mode="r") as trollface:
+    with open(str(f"{os.path.dirname(os.path.abspath(__file__))}{trollface_path}"), mode="r") as trollface:
         print(trollface.read())
